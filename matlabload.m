adec7e6a28327b4955b599f5957f80caa4a47c50
David Bradway <dpb6@duke.edu>
2022-11-21 15:14:34 -0500

    improve formatting

--- matlabload.m
+++ matlabload.m
@@ -1,9 +1,8 @@
 function [my_struct] = matlabload(filename)
     if nargin == 0
-        filename ='/getlab/dpb6/repos/json_example/data.json';
+        filename = fullfile(pwd, 'data.json');
     end
     
     filetext = fileread(filename);
     my_struct = jsondecode(filetext);
-    disp(my_struct)
 end
--- pythonload.py
+++ pythonload.py
@@ -1,12 +1,10 @@
 import json
 from pprint import pprint
-# http://www.json.org/
 
 def main(filename = 'data.json'):
 	with open(filename) as data_file:
 	    data = json.load(data_file)
-	pprint(data)
 	return data
 
 if __name__ == '__main__':
-	main()
+	pprint(main())
